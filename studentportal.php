7b932dac06000635240f1084c9dcbdca5bfbf40a
Manohar1503 <105846844+Manohar1503@users.noreply.github.com>
2023-09-23 23:54:26 +0530

    Add files via upload

---
+++ studentportal.php
@@ -0,0 +1,127 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="flickity/flickity.css">
+	<script type="text/javascript" src="flickity/flickity.js"></script>
+	<title>Library Management</title>
+
+</head>
+<body>
+<div class="container">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example">
+					<span class="sr-only">:</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">SheddyBen Lib-BookSys</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="profile.php">View Profile</a></li>
+					<li><a href="borrow-student.php">Borrow Books</a></li>					
+					<li><a href="fine-student.php">Fines</a></li>					
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</div>
+
+		<div class="container-fluid slide">
+			
+	  		<div class="slider">
+	  			<!-- <h1>Flickity - wrapAround</h1> -->
+
+
+					<div class="carousel" data-flickity='{ "autoPlay": true }'; >
+
+						  <div class="carousel-cell" auto-play >
+						  	<img src="images/header-bg.jpg">
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	<img src="images/header-bg.jpg">
+
+						  </div>
+						  
+					</div>
+
+					
+
+	  		</div>
+		</div>
+
+		<div class="container slide2">
+			
+	  		<div class="row">
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>Welcome to our portal</h2>
+		  			</div>
+	  				
+	  				<a href="#"><p class="slide2"><button class="btn btn-success">Sign Up</button></p></a>
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>24/7 Operational Support</h2>
+		  			</div>
+	  				It is important to take care of the pain itself, and it will be followed by the growth of the patient, 
+					but at the same time it will happen that there is a lot of work and pain.For to come to the smallest detail, 
+					no one should practice any kind of work unless he derives some benefit from it. Do not be angry with the pain 
+					in the reprimand in the pleasure he wants to be a hair from the pain in the hope that there is no breeding.
+					Unless they are blinded by lust, they do not come forth; they are in fault who abandon their duties and soften their hearts, 
+					that is, their labors.
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+	  				<div class="page-header col-lg-offset-1">
+	  				<h2>Why Us?</h2>
+	  			</div>
+	  				
+	  			</div>
+	  		</div>
+		</div>
+
+		<div class="container-fluid slide3" style="background-color: #282828">
+			<div class="container">
+				<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				
+			</div>
+			</div>
+			
+		</div>
+		<div class="container_fluid">
+			<p class="page-header center-block">
+			It's very important to have a patient, and it will be followed by an adipiscing process, but I'll give it that way
+				
+			</p>
+			<p class="page-header center-block">
+				<a href="#"><button class="btn btn-primary">Contact Us</button></a>
+				<a href="#"><button class="btn btn-info">About Us</button></a>
+
+			</p>
+		</div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
